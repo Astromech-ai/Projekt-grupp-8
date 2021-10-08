a48cc0d43f0a77fec131c2792fafd67dd7661058
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2021-10-08 14:21:40 +0200

    Movement
    Rymdskepp kan flytta sig

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  Movement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float Speed = 10.0f; //Hastigheten vi rör oss/Timothy
+
+
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();    //Hämtar den komponent vi vill använda/Timothy
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * Speed, vertical * Speed);
+    }
+}
