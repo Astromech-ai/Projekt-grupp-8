a318b2763dbe383de414248152e994360b084c5b
plombs <plombs@users.noreply.github.com>
2021-10-08 14:22:20 +0200

    Main Menu

---
+++ Assets/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Byter scen till nästa
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //Stänger ner spelet
+        print("Working"); //För att se om den fungerar
+    }
+}
