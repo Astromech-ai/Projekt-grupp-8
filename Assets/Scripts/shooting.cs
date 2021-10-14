b18f7fe8c9bb896205e14cb82205ce41e97fe1e7
Astromech-ai <79506266+Astromech-ai@users.noreply.github.com>
2021-10-14 14:14:49 +0200

    Shooting

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject BulletPrefab;
+    // Update is called once per frame
+    void Update()   //om man trycker skjut knappen så aktiveras Shoot
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    
+
+    void Shoot()
+    {
+        Instantiate(BulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+}
