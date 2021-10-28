5ea308decf51ededdacb589c971f83011358e6fe
v9z <v9z@users.noreply.github.com>
2021-10-28 14:54:11 +0200

    Score + Save

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -9,12 +9,15 @@ public class Score : MonoBehaviour
     public int score;
     public Text scoreText;
 
+    public int Highscore;
+    public Text HighscoreText;
+
     public bool test;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Highscore = PlayerPrefs.GetInt("Highscore", Highscore);  //Laddar highscoren
     }
 
     // Update is called once per frame   
@@ -23,9 +26,18 @@ public class Score : MonoBehaviour
         scoreText.text = score + ""; //Ändrar score texten
 
 
-        if(test == true)
+        if(test == true)  //Lägger till 1 i score när man skjuter en alien
         {
-
+            score = score + 1;
         }
+
+        if(score > Highscore)  //Ändrar highscoren
+        {
+            Highscore = score;
+        }
+
+        HighscoreText.text = "Highscore " + Highscore;  //Ändrar highscore texten
+
+        PlayerPrefs.SetInt("Highscore", Highscore);  //Sparar highscoren
     }
 }
